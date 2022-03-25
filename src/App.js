a210049e4b9b1f845d13fc7fd080f06144b887de
Lrd134 <larryc3200@gmail.com>
2022-03-25 18:28:23 -0400

    added store and middleware

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import logo from './logo.svg';
 import './App.css';
-
+const store = compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__
+  ? window.__REDUX_DEVTOOLS_EXTENSION__()
+  : f => f,
+)(createStore)(profileReducer);
 class App extends Component {
   componentDidMount() {
     fetch("https://api.hatchways.io/assessment/students").then(
