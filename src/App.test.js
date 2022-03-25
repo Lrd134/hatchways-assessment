b25b6ba8db35ec50535583a7157daa8e8bf51ae3
Lrd134 <larryc3200@gmail.com>
2022-03-25 18:17:25 -0400

    changed testing library

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
 test('renders characters', () => {
-  const component = renderer.create(
-  );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  render(<App />);
+  const emailElement = screen.getByText(/email/i);
+  expect(emailElement.className).toBe("profile")
 });
