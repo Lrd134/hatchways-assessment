2e3ad56a37fd7d84ec90ee5d6967faca90aa4774
Lrd134 <larryc3200@gmail.com>
2022-03-25 18:18:43 -0400

    fetching api data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    fetch("https://api.hatchways.io/assessment/students").then(
+      resp => resp.json()
+    ).then(
+      json => updateUsers(json)
+    )
+  }
   render() {
     return (
       <div className="App">
