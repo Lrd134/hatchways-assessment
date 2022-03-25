7ee14711750ba965ef0eb47119766a083244c894
Lrd134 <larryc3200@gmail.com>
2022-03-25 18:44:46 -0400

    fixed function call

--- src/ProfileContainer.js
+++ src/ProfileContainer.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 class ProfileContainer extends Component {
+
+  componentDidMount() {
+    fetch("https://api.hatchways.io/assessment/students").then(
+      resp => resp.json()
+    ).then(
+      json => this.props.getUsers(json)
+    )
+  }
+
   render() {
     return (
       <div className="profile container">
