aa947e314773f9f13e6b4063166ae57b00909f58
Lrd134 <larryc3200@gmail.com>
2022-03-25 22:39:33 -0400

    rendered grades when button is pressed

--- src/Profile.js
+++ src/Profile.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Profile.css';
 
 function Profile({user}) {
   const avgGrades = gradeArr => (gradeArr.reduce((prevVal, currVal) =>
     parseFloat(prevVal) + parseFloat(currVal)) / gradeArr.length).toFixed(2)
-
+  let [checked, setChecked] = useState(false);
+  const onCheck = event => {
+    event.preventDefault();
+    setChecked(!checked);
+  }
+  const renderGrades = () => user.grades.map((grade, index) => (<p className="grades">Test {index + 1}:<span className="tab"></span>{grade}%</p>))
   return (
     <div className="profile">
       <img src={user.pic}/>
-      <button className="profile"/>
+      <button className="profile" onClick={onCheck}/>
+      
       <div className="profile information">
       <h1>{user.firstName} {user.lastName}</h1>
       <p>Email: {user.email}</p>
@@ -16,6 +22,8 @@ function Profile({user}) {
       <p>Skill: {user.skill}</p>
       <p>Average: {avgGrades(user.grades)}</p>
       </div>
+      
+      {checked ? (renderGrades()) : null}
     </div>
   )
 }
