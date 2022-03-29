c2e3d1a759c41de87fcf151e296da42395344a62
Lrd134 <larryc3200@gmail.com>
2022-03-28 20:34:41 -0400

    handled new tags

--- src/profileReducer.js
+++ src/profileReducer.js
@@ -8,6 +8,15 @@ const profileReducer = (state = {
     case("GET_USERS"):
       return { ...state, users: action.payload.students, requesting: false}
     case("NEW_TAG"): {
+      for (let profile of state.users) {
+        function noTags() {
+          profile.tags = []
+          profile.tags.push(action.payload.tag)
+        }
+        if (profile.firstName === action.payload.name) {
+          profile.tags !== undefined ? profile.tags.push(action.payload.tag) : noTags();
+        }
+      }
       return {...state, requesting: false}
     }
     default:
